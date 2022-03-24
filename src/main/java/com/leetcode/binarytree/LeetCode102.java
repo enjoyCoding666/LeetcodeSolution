0bc0a87d2927f541ceddf6f6778de7b0fd7d70c3
fenglin <huaiyinnianshao28@163.com>
2022-03-25 00:26:28 +0800

    二叉树的层序遍历，广度优先搜索算法BFS

---
+++ src/main/java/com/leetcode/binarytree/LeetCode102.java
@@ -0,0 +1,51 @@
+package com.leetcode.binarytree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+
+public class LeetCode102 {
+
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        if (root == null) {
+            return resultList;
+        }
+        //将树节点放入队列中
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            //树的每一层的节点数量，就是队列的size
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+
+            while (size-- > 0) {
+                TreeNode treeNode = queue.poll();
+                if (treeNode == null) {
+                    continue;
+                }
+                list.add(treeNode.val);
+                //把下一层的左/右节点加到队列中
+                TreeNode left = treeNode.left;
+                if (left != null) {
+                    queue.offer(left);
+                }
+                TreeNode right = treeNode.right;
+                if (right != null) {
+                    queue.offer(right);
+                }
+            }
+
+            resultList.add(list);
+
+        }
+        return resultList;
+
+    }
+
+
+}
