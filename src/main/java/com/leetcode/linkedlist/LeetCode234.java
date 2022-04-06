81313afc5afa9030039f5e12aa705cc9db41201e
fenglin <huaiyinnianshao28@163.com>
2022-04-07 00:05:40 +0800

    链表转集合，判断该链表是否为回文链表

--- src/main/java/com/leetcode/linkedlist/LeetCode234.java
+++ src/main/java/com/leetcode/linkedlist/LeetCode234.java
@@ -1,6 +1,9 @@
 package com.leetcode.linkedlist;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
  * <p>
@@ -15,16 +18,17 @@ public class LeetCode234 {
         if (head == null) {
             return true;
         }
-        int[] nodes = new int[1000];
-        int t = 0;
+        List<Integer> list = new ArrayList<>();
         //迭代链表，将数据复制到数组
         while (head != null) {
-            nodes[t++] = head.val;
+            list.add(head.val);
             head = head.next;
         }
         //判断是否回文
-        for (int i = 0; i < t; i++) {
-            if (nodes[i] != nodes[t - 1 - i]) {
+        for (int i = 0; i < list.size(); i++) {
+            Integer left = list.get(i);
+            Integer right = list.get(list.size() - 1 - i);
+            if (!left.equals(right)  ) {
                 return false;
             }
         }
