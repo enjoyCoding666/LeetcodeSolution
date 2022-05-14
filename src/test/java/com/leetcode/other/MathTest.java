34f7467c4873f701981b75d505a12a913041d01c
fenglin <huaiyinnianshao28@163.com>
2022-05-14 22:25:01 +0800

    把数字出现的次数放到map中，判断是否出现次数为0

---
+++ src/test/java/com/leetcode/other/MathTest.java
@@ -0,0 +1,10 @@
+package com.leetcode.other;
+
+public class MathTest {
+    public static void main(String[] args) {
+
+        int min = Math.min(2, 3);
+        int max = Math.max(2, 3);
+        int abs = Math.abs(2 - 3);
+    }
+}
