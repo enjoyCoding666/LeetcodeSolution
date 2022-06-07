741adec8d47f403038003b7a271b044f6f215b29
fenglin <huaiyinnianshao28@163.com>
2022-06-07 23:18:17 +0800

    动态规划，空间换时间。

---
+++ src/main/java/com/leetcode/dynamicprogramming/LeetCode70Recursion.java
@@ -0,0 +1,27 @@
+package com.leetcode.dynamicprogramming;
+
+public class LeetCode70Recursion {
+    /**
+     *
+     * 爬楼梯的递归解法。
+     * 执行时间随着 n的增大，指数级上升。
+     *
+     * 以此可以对比，动态归划的"空间换时间"。
+     *
+     */
+    public int climbStairs(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+
+        return climbStairs(n - 1) + climbStairs(n - 2);
+
+
+    }
+}
